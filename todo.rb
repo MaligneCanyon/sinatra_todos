b7067f13afcac3389a0957ea0ae7e9c49a3a1d31
MaligneCanyon <grantkaratnyk@hotmail.com>
2019-11-12 15:57:30 -0700

    minor chgs to todo.rb

--- todo.rb
+++ todo.rb
@@ -10,6 +10,7 @@ require "tilt/erubis"
 configure do
   enable :sessions
   set :session_secret, 'secret'
+  set :erb, :escape_html => true
 end
 
 # helpers should accept a Todo obj as input; sim to instance
